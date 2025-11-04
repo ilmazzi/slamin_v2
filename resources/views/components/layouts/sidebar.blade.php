f175c75b504b673ab2b400765b4281fd3ff68af5
ilmazzi <davide.mazzitelli@yahoo.it>
2025-11-04 07:51:33 +0100

    🔧 FIX: Sostituite classi delay personalizzate con quelle predefinite Tailwind per compatibilità

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -180,7 +180,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                         </svg>
                         <span x-show="!collapsed" 
-                              x-transition:enter="transition ease-out duration-300 delay-[250ms]"
+                              x-transition:enter="transition ease-out duration-300 delay-200"
                               x-transition:enter-start="opacity-0 -translate-x-4"
                               x-transition:enter-end="opacity-100 translate-x-0"
                               x-transition:leave="transition ease-in duration-200"
@@ -205,7 +205,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/>
                         </svg>
                         <span x-show="!collapsed" 
-                              x-transition:enter="transition ease-out duration-300 delay-[300ms]"
+                              x-transition:enter="transition ease-out duration-300 delay-300"
                               x-transition:enter-start="opacity-0 -translate-x-4"
                               x-transition:enter-end="opacity-100 translate-x-0"
                               x-transition:leave="transition ease-in duration-200"
@@ -230,7 +230,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                         </svg>
                         <span x-show="!collapsed" 
-                              x-transition:enter="transition ease-out duration-300 delay-[350ms]"
+                              x-transition:enter="transition ease-out duration-300 delay-300"
                               x-transition:enter-start="opacity-0 -translate-x-4"
                               x-transition:enter-end="opacity-100 translate-x-0"
                               x-transition:leave="transition ease-in duration-200"
@@ -256,7 +256,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
                         </svg>
                         <span x-show="!collapsed" 
-                              x-transition:enter="transition ease-out duration-300 delay-[400ms]"
+                              x-transition:enter="transition ease-out duration-300 delay-500"
                               x-transition:enter-start="opacity-0 -translate-x-4"
                               x-transition:enter-end="opacity-100 translate-x-0"
                               x-transition:leave="transition ease-in duration-200"
@@ -281,7 +281,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                         </svg>
                         <span x-show="!collapsed" 
-                              x-transition:enter="transition ease-out duration-300 delay-[450ms]"
+                              x-transition:enter="transition ease-out duration-300 delay-500"
                               x-transition:enter-start="opacity-0 -translate-x-4"
                               x-transition:enter-end="opacity-100 translate-x-0"
                               x-transition:leave="transition ease-in duration-200"
