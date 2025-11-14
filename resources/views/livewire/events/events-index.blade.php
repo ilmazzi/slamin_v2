1205d69d8c5ffc54f7099fa243fc7e2f52874b47
ilmazzi <davide.mazzitelli@yahoo.it>
2025-11-14 11:19:45 +0100

    Fix active map button hover color

--- resources/views/livewire/events/events-index.blade.php
+++ resources/views/livewire/events/events-index.blade.php
@@ -151,7 +151,7 @@
                         <button 
                             onclick="changeMapStyle('voyager')"
                             id="style-voyager"
-                            class="map-style-btn active p-2.5 rounded-full hover:bg-primary-50 dark:hover:bg-primary-900/20 transition-all"
+                            class="map-style-btn active p-2.5 rounded-full hover:bg-red-50 dark:hover:bg-red-900/20 transition-all"
                             title="Mappa Colorata">
                             <svg class="w-4 h-4 text-red-600 dark:text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>
