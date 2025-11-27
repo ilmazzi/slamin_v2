4852f219bd31d242ab4788766f5660a458428538
ilmazzi <davide.mazzitelli@yahoo.it>
2025-11-27 14:17:55 +0100

    Update mobile logo to use new loghino files with dark mode support

--- resources/views/components/layouts/topbar.blade.php
+++ resources/views/components/layouts/topbar.blade.php
@@ -19,10 +19,14 @@
 
             <!-- Logo (Responsive: piccolo su mobile, orizzontale su desktop) -->
             <a href="{{ route('home') }}" class="flex items-center">
-                <!-- Logo piccolo mobile -->
-                <img src="{{ asset('assets/images/logo.png') }}" 
+                <!-- Logo piccolo mobile - Light mode -->
+                <img src="{{ asset('assets/images/loghino-nerosubianco.png') }}" 
                      alt="{{ config('app.name') }}" 
-                     class="h-10 w-10 md:hidden object-contain">
+                     class="h-10 w-10 md:hidden dark:hidden object-contain">
+                <!-- Logo piccolo mobile - Dark mode -->
+                <img src="{{ asset('assets/images/loghino-biancosunero.png') }}" 
+                     alt="{{ config('app.name') }}" 
+                     class="h-10 w-10 hidden dark:block md:dark:hidden object-contain">
                 <!-- Logo orizzontale desktop - Light mode -->
                 <img src="{{ asset('assets/images/Logo_orizzontale_nerosubianco.png') }}" 
                      alt="{{ config('app.name') }}" 
